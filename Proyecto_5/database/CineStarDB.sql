cfd8fb6b23c535c97fd958d39c697ccf4a52638d
LENOVO <LENOVO@DESKTOP-3DA24H6>
2026-04-22 18:24:30 -0500

    Avance 2 1.con conexion a sql server 2. Archivo sql para descargar

--- Proyecto_5/database/CineStarDB.sql
+++ Proyecto_5/database/CineStarDB.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE CineStarDB;
 GO
+
 USE CineStarDB;
 GO
 
@@ -61,3 +62,41 @@ CREATE TABLE HistorialCancelaciones (
     FOREIGN KEY (idReserva) REFERENCES Reserva(idReserva),
     FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
+
+INSERT INTO Sala (nombre, capacidad) VALUES
+('Sala 1', 100),
+('Sala 2', 100),
+('Sala 3', 100);
+
+INSERT INTO Pelicula (titulo, duracionMin, clasificacion) VALUES
+('Avengers', 180, 'PG13'),
+('Spiderman', 140, 'PG13'),
+('Toy Story', 100, 'G');
+
+INSERT INTO Funcion (idSala, idPelicula, fechaHora, cupoDisponible) VALUES
+(1, 1, '2026-04-23 18:00', 100),
+(2, 2, '2026-04-23 20:00', 100),
+(3, 3, '2026-04-23 22:00', 100);
+
+DECLARE @sala INT = 1;
+
+WHILE @sala <= 3
+BEGIN
+    DECLARE @fila CHAR(1) = 'A';
+    WHILE @fila <= 'J'
+    BEGIN
+        DECLARE @num INT = 1;
+        WHILE @num <= 10
+        BEGIN
+            INSERT INTO Asiento (idSala, fila, numero)
+            VALUES (@sala, @fila, @num);
+
+            SET @num = @num + 1;
+        END
+        SET @fila = CHAR(ASCII(@fila) + 1);
+    END
+    SET @sala = @sala + 1;
+END;
+
+CREATE INDEX idx_reserva_funcion
+ON Reserva(idFuncion, estado);
