fe68dc2424b9c65061c7e85de32910506b5bf78f
LENOVO <LENOVO@DESKTOP-3DA24H6>
2026-04-23 20:55:37 -0500

    Avance 2 1. con conexion a sql server 2. Archivo sql para descargar 3. clases modificadas para mejero resultado 4. dao (la verdadera conexion con base de datos) 5. service 6. ejecutable beta de el main principal, se reemplazara al hacer lo de view

--- Proyecto_5/src/main/java/com/mycompany/proyecto_5/Proyecto_5.java
+++ Proyecto_5/src/main/java/com/mycompany/proyecto_5/Proyecto_5.java
@@ -3,6 +3,9 @@
  */
 
 package com.mycompany.proyecto_5;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -11,6 +14,15 @@ package com.mycompany.proyecto_5;
 public class Proyecto_5 {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        JFrame ventana = new JFrame("Sistema de Reservas CineStar");
+
+        ventana.setSize(500,300);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+
+        JLabel texto = new JLabel("Sistema de Reservas CineStar", SwingConstants.CENTER);
+        ventana.add(texto);
+
+        ventana.setVisible(true);
     }
 }
