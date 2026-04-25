94ab6336491b5426aa9aea7b3e5f49cda8385560
LENOVO <LENOVO@DESKTOP-3DA24H6>
2026-04-25 16:15:14 -0500

    Avance 2 1. con conexion a sql server 2. Archivo sql para descargar 3. clases modificadas para mejero resultado 4. dao (la verdadera conexion con base de datos) 5. service 6. ejecutable a medias del main principal 7. cosas agregadas al view

--- Proyecto_5/src/main/java/com/mycompany/proyecto_5/Proyecto_5.java
+++ Proyecto_5/src/main/java/com/mycompany/proyecto_5/Proyecto_5.java
@@ -4,8 +4,6 @@
 
 package com.mycompany.proyecto_5;
 import com.mycompany.proyecto_5.view.FrmPrincipal;
-import com.mycompany.proyecto_5.view.FrmFunciones;
-import com.mycompany.proyecto_5.view.FrmReservas;
 
 /**
  *
@@ -13,22 +11,10 @@ import com.mycompany.proyecto_5.view.FrmReservas;
  */
 public class Proyecto_5 {
 
-    public static void main(String[] args) {
-
-        try {
-            javax.swing.UIManager.setLookAndFeel(
-                javax.swing.UIManager.getSystemLookAndFeelClassName()
-            );
-        } catch (Exception e) {
-            System.out.println("No se pudo aplicar el LookAndFeel");
-        }
-
-        java.awt.EventQueue.invokeLater(() -> {
-            new FrmPrincipal().setVisible(true);
-            new FrmFunciones().setVisible(true);
-            new FrmReservas().setVisible(true);
-        });
-    }
-
+   public static void main(String[] args) {
         
+        FrmPrincipal frm = new FrmPrincipal();
+        frm.setLocationRelativeTo(null); // centra la ventana
+        frm.setVisible(true);       
+    }       
 }
